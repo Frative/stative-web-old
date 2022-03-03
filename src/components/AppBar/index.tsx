1ec7365e845ad35c2c468bdf48d7833a25a395fb
Christopher <nealthmc@gmail.com>
2022-03-03 18:38:51 -0500

    menu icon to AppBar

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,12 +1,27 @@
+// region import
 import React from 'react'
-import { AppBar as MUIAppBar, Box, Toolbar } from '@mui/material'
+import {
+  AppBar as MUIAppBar, Box, Toolbar, IconButton, Typography,
+} from '@mui/material'
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined'
 
 function AppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <MUIAppBar position="static">
         <Toolbar>
-          AppBar
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuOutlinedIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {process.env.REACT_APP_NAME}
+          </Typography>
         </Toolbar>
       </MUIAppBar>
     </Box>
