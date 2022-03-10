c6df85dd58a17770bd31d61698efe2426a3b2b58
Christopher <nealthmc@gmail.com>
2022-03-10 15:05:44 -0500

    remove menu

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -1,18 +1,14 @@
 // region import
 import React from 'react'
 import {
-  AppBar as MUIAppBar, Box, Toolbar, IconButton, Typography, Drawer,
+  AppBar as MUIAppBar, Box, Toolbar, Typography, Drawer,
 } from '@mui/material'
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined'
 
 function AppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <MUIAppBar position="static">
         <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuOutlinedIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {process.env.REACT_APP_NAME}
           </Typography>
